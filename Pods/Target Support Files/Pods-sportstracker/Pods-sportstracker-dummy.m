113f32b98d3f4f6c22a20223288bf30873cb412b
Rene <rene.windischhofer@aon.at>
2016-10-17 10:10:56 +0200

    adding pod

---
+++ Pods/Target Support Files/Pods-sportstracker/Pods-sportstracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sportstracker : NSObject
+@end
+@implementation PodsDummy_Pods_sportstracker
+@end
---
+++ Pods/Target Support Files/Pods-sportstracker/Pods-sportstracker-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_sportstrackerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_sportstrackerVersionString[];
+
